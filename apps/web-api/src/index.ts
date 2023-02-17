4aadbaab6e1ad0bd7d397ee58b3cab10b72f2d3e
Marc Troisner <marc.troisner@gmail.com>
2023-02-17 13:32:43 +0100

    build(web-api, ts-config, eslint-config): Basic express app setup and with build/dev tools, node eslint config, typescript node config

---
+++ apps/web-api/src/index.ts
@@ -0,0 +1,13 @@
+import type { Request, Response } from 'express';
+
+import express from 'express';
+
+const app = express();
+
+app.get('/', (_req: Request, res: Response): void => {
+  res.send({ root: 'web-api' });
+});
+
+app.listen(8000, (): void => {
+  console.log(`App listening on port ${8000}`);
+});
---
+++ packages/eslint-config/node.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+    'airbnb-typescript/base'
+  ],
+  env: {
+    es2021: true,
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {}
+}
