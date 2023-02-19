04b79a9d3a2dd49f156194eff48ccb1b4ee87dc6
Marc Troisner <marc.troisner@gmail.com>
2023-02-19 11:55:32 +0100

    fix(web-api): Removed testing middleware

--- apps/web-api/src/index.ts
+++ apps/web-api/src/index.ts
@@ -1,37 +1,16 @@
 import type { Request, Response } from 'express';
 
-import Joi from 'joi';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import { json } from 'body-parser';
-import { httpLogger, appLogger, bodyValidator, queryValidator, headerValidator, cookieValidator } from '@package/middleware';
 
 const app = express();
 
 app.use(json());
 app.use(cookieParser());
-app.use(httpLogger());
-app.use(appLogger);
 
-const schema = Joi.object({
-  foo: Joi.string().required(),
-  bar: Joi.number().min(10).max(20).optional(),
-});
-
-const validators = [
-  bodyValidator(schema),
-  queryValidator(schema),
-  headerValidator(schema),
-  cookieValidator(schema),
-];
-
-app.get('/:id?', validators, (req: Request, res: Response): void => {
-  res.send({ root: 'web-api',
-    body: req.body,
-    cookies: req.cookies,
-    query: req.query,
-    headers: req.headers,
-  });
+app.get('/', (req: Request, res: Response): void => {
+  res.send({ root: 'web-api' });
 });
 
 app.listen(8000, (): void => {
